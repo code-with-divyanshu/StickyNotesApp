3598438369dffd931b6281bf94bf9938ad393ac2
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-14 22:43:53 +0530

    Use Context Api in Frontent

--- src/pages/NotesPage.jsx
+++ src/pages/NotesPage.jsx
@@ -1,25 +1,9 @@
-import { useEffect, useState } from "react";
-// import { fakeData as notes } from "../assets/fakeData";
 import NoteCard from "../components/NoteCard";
-import { db } from "../appwrite/databases";
+import { useContext } from "react";
+import { NoteContext } from "../context/NoteContext";
 
 const NotesPage = () => {
-  const [notes, setNotes] = useState([]);
-
-  useEffect(() => {
-    init();
-  }, []);
-
-  const init = async () => {
-    const response = await db.notes.list();
-    // const response = await databases.listDocuments(
-    //   import.meta.env.VITE_DATABASE_ID,
-    //   import.meta.env.VITE_COLLECTION_NOTES_ID
-    // );
-
-    setNotes(response.documents);
-  };
-
+  const { notes } = useContext(NoteContext);
   return (
     <div>
       {notes.map((note) => (
