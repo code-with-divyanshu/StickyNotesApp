91e1f19efc5f0a269ef07296c260c58971626cfc
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-16 22:37:11 +0530

    Create Controls Component

---
+++ src/components/Controls.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AddButton from "./AddButton";
+
+const Controls = () => {
+  return (
+    <div id="controls">
+      <AddButton />
+    </div>
+  );
+};
+
+export default Controls;
