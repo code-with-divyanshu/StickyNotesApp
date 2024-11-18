4da71aec4c05c1344cfcd78e29ef60139e50f926
code-with-divyanshu <divyanshubartwal67@gmail.com>
2024-11-18 21:41:17 +0530

    Create Controls for Color

--- src/components/Controls.jsx
+++ src/components/Controls.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import AddButton from "./AddButton";
+import Colors from "../assets/colors.json";
+import Color from "./Color";
 
 const Controls = () => {
   return (
     <div id="controls">
       <AddButton />
+      {Colors.map((color) => (
+        <Color key={color.id} color={color} />
+      ))}
     </div>
   );
 };
